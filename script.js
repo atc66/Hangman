194c4a0dabed8a97a0c3cd2271ff18b266773448
aciukurescu <andrewciukurescu@gmail.com>
2018-02-01 16:08:58 -0500

    word bank and random word creator

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('test')
+
+// Our Possible Answers
+var answer = ["cat", "dog", "bird"]
+
+// Randomly Generate a word to guess
+var randomAnswer = answer[Math.floor(Math.random() * answer.length)];
+console.log(randomAnswer);
+
+
+function guessLetter(x){
+
+}
