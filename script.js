18a42611e5467eba7dd66a91a1ee58c5d01008c7
aciukurescu <andrewciukurescu@gmail.com>
2018-02-01 16:36:03 -0500

    shows blank spaces for the answers amount of letters

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-console.log('test')
+var blankAnswer = document.getElementById('blankAnswer');
 
 // Our Possible Answers
-var answer = ["cat", "dog", "bird"]
+var words = ["cat", "dog", "bird", "turtle", "iguana", "lizard", "fish", "horse", "mouse", "gerbil", "hamster"]
 
 // Randomly Generate a word to guess
-var randomAnswer = answer[Math.floor(Math.random() * answer.length)];
+var randomAnswer = words[Math.floor(Math.random() * words.length)];
 console.log(randomAnswer);
 
-
-function guessLetter(x){
-
+// Where the answer will be displayed as blank spaces
+var answer = [];
+// for loop to generate the amount of spaces needed for the answer to be displayed
+for (i=0; i < randomAnswer.length; i++){
+	answer[i] = " _ ";
 }
+// shows the underscores representing the right letter
+console.log(answer)
+
+// Display the blank spaces on the screen
+blankAnswer.innerHTML = answer;
