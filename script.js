1a3b9824085a1e20f2f0f9f24051e6fce3b38fc2
aciukurescu <andrewciukurescu@gmail.com>
2018-02-02 17:40:40 -0500

    letters guessed are stored, letters correct are shown, there is still an issue with words that have multiples of the same letter

--- script.js
+++ script.js
@@ -1,52 +1,48 @@
 var blankAnswer = document.getElementById('blankAnswer');
 var submit = document.getElementById('try');
+var guess = document.getElementById('playerGuess');
 var letterGuessed = document.getElementById('letterGuessed');
-
-
-// Our Possible Answers
-var words = ["cat", "bird", "horse"]
-
-// Randomly Generate a word to guess
-var word = words[Math.floor(Math.random() * words.length)];
-console.log(word);
+var words = ["cat", "bird", "horse", "banana"]
+var letterGuess = [];
+var answer = words[Math.floor(Math.random() * words.length)];
+console.log(answer);
 
 // Where the answer will be displayed as blank spaces
-var answer = [];
-// for loop to generate the amount of spaces needed for the answer to be displayed
-for (i=0; i < word.length; i++){
-	answer[i] = " _ ";
+var blanks = [];
+for (i=0; i < answer.length; i++){
+	blanks[i] = " _ ";
 }
-console.log(answer)
+console.log(blanks)
 
 // Display the blank spaces on the screen, .join removes the commas
-blankAnswer.innerHTML = answer.join(' ');
-
-var letterGuess = [];
-
-var availableLetters = "qwertyuiopasdfghjklzxcvbnm";
-	// console.log(availableLetters[1]);
+blankAnswer.innerHTML = blanks.join(' ');
 
 // click submit to guess a letter after typing one in
 submit.addEventListener('click', function(){
-	// function for checking guess
-	// Letters to pick from
-	// var availableLetters = "qwertyuiopasdfghjklzxcvbnm";
-	// links the guess to what is typed in the box
 	var guess = document.getElementById('playerGuess').value;
-		for (i=0; i <= word.length; i++) {
-		if (guess.length !== 1) {
-			alert("Player can only guess one letter at a time!");
-			break;
-		} else if (guess !== word[i]){
-			console.log("There are no " + guess + "'s guess again")
-		} else {
-			alert("There is a " + guess);	
+	for (i=0; i <= answer.length; i++) {
+		if (guess == answer[i]){
+			console.log("this is correct")
+			blanks[i] = guess
+			blankAnswer.innerHTML = blanks.join(' ');
+			letterGuess.push(guess)
+			letterGuessed.innerHTML = letterGuess;
+			return
 		}
-		letterGuess.push(guess);
 	}
-	letterGuessed.innerHTML = letterGuess;
-})
 
+	for (j=0; j <= answer.length; j++) {
+		if( guess !== answer[i]){
+			console.log('this is incorrect')
+			letterGuess.push(guess)
+			letterGuessed.innerHTML = letterGuess;
+			return
+		}
+	}
+})
+	
+
+	
 
 
 
