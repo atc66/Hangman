edc4e964a0709b14bbab3cc35299da5fda1f3666
aciukurescu <andrewciukurescu@gmail.com>
2018-02-02 14:43:33 -0500

    you can now guess one letter at a time and prompts reply if theyre correct or not

--- script.js
+++ script.js
@@ -21,35 +21,32 @@ console.log(answer)
 // Display the blank spaces on the screen, .join removes the commas
 blankAnswer.innerHTML = answer.join(' ');
 
+
+
+var availableLetters = "qwertyuiopasdfghjklzxcvbnm";
+	// console.log(availableLetters[1]);
+
 // click submit to guess a letter after typing one in
 submit.addEventListener('click', function(){
+	// function for checking guess
 	// Letters to pick from
 	var availableLetters = "qwertyuiopasdfghjklzxcvbnm";
 	// links the guess to what is typed in the box
 	var guess = document.getElementById('playerGuess').value;
-	// guess needs to be only one letter
-	// 
-	// if (guess.length !== 1) {
-	// 	alert("Player can only guess one letter at a time!");
-	// } else if (guess !== availableLetters) {
-	// 	alert("Only enter letters A thru Z");
-	// }	else 
-
-	if (guess === word) {
-		alert("I have a " + guess);
-	} else if (guess !== word) {
-		alert ("I do not have a " + guess)
+		for (i=0; i <= word.length; i++){
+		if (guess == word[i]){
+			alert("There is a " + word[i]);
+			break;
+		} else if (guess.length !== 1) {
+			alert("Player can only guess one letter at a time!");
+			break;
+		} else {
+			alert("There are no " + guess + "'s guess again")
+			break;
+		}
 	}
+})
 
-	// console.log(guess.value)
-	//  		// Update the game state with the guess
-	//  	for (var j = 0; j < word.length; j++) {
-	//  		if (word[j] === guess) {
-	//  			answer[j] = guess;
-	//  		}
-	//  	}
-	// }	
-})	
 
 
 
