b50ed062001ce685bc60f0eb7b88f8dfc7dc16fd
aciukurescu <andrewciukurescu@gmail.com>
2018-02-03 13:30:54 -0500

    started to add hints

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ var blankAnswer = document.getElementById('blankAnswer');
 var submit = document.getElementById('try');
 var guess = document.getElementById('playerGuess');
 var letterGuessed = document.getElementById('letterGuessed');
+var hint = document.getElementById('hint');
+// var hintIndex = indexOf(answer);
 var words = ["cat", "bird", "horse", "banana"]
-var letterGuess = [];
 var answer = words[Math.floor(Math.random() * words.length)];
 console.log(answer);
+var finalAnswer = answer.split('')
+var letterGuess = [];
+
+
+var hints = ['meow', 'chirp', 'nay', 'monkey food']
 
 // Where the answer will be displayed as blank spaces
 var blanks = [];
@@ -13,36 +19,59 @@ for (i=0; i < answer.length; i++){
 	blanks[i] = " _ ";
 }
 console.log(blanks)
-
 // Display the blank spaces on the screen, .join removes the commas
 blankAnswer.innerHTML = blanks.join(' ');
 
 // click submit to guess a letter after typing one in
 submit.addEventListener('click', function(){
 	var guess = document.getElementById('playerGuess').value;
-	for (i=0; i <= answer.length; i++) {
-		if (guess == answer[i]){
+	var bool = false;
+	for (i=0; i <= finalAnswer.length; i++) {
+		if (guess === finalAnswer[i]){
 			console.log("this is correct")
 			blanks[i] = guess
 			blankAnswer.innerHTML = blanks.join(' ');
 			letterGuess.push(guess)
+			numberRightGuesses.push(guess)
 			letterGuessed.innerHTML = letterGuess;
-			return
+			bool = true;
 		}
 	}
 
-	for (j=0; j <= answer.length; j++) {
-		if( guess !== answer[i]){
+	if(!bool){
+	for (j=0; j <= finalAnswer.length; j++) {
+		if( guess != finalAnswer[i]){
 			console.log('this is incorrect')
 			letterGuess.push(guess)
+			numberWrongGuesses.push(guess)
 			letterGuessed.innerHTML = letterGuess;
 			return
+			}
 		}
-	}
+	}	
 })
-	
 
-	
+
+var numberWrongGuesses = [];
+
+function wrongGuessFunction(){
+	if(numberWrongGuesses > 3) {
+		alert("Game Over")
+	}
+}	
+var numberRightGuesses = [];	
+function rightGuessFunction(){
+	if(numberRightGuesses.length = 3) {
+		alert("Game Over")
+	}
+}	
+
+
+
+hint.addEventListener('click', function(){
+	var hintIndex = indexOf(word);
+    hint.innerHTML = "Clue: - " +  hints[hintIndex];
+})
 
 
 
