2b695bca73a190b7ef03ea8c86d22a786119751a
aciukurescu <andrewciukurescu@gmail.com>
2018-02-04 00:56:02 -0500

    game now displays alert when won or lost, letters on counted once on multi letter answers

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ var guess = document.getElementById('playerGuess');
 var letterGuessed = document.getElementById('letterGuessed');
 var hint = document.getElementById('hint');
 var reset = document.getElementById('reset');
-
-// var hintIndex = indexOf(answer);
-var words = ["cat", "bird", "horse", "phone", "candle", "stamp", "wonder", "hockey", "thursday", "friday", "fruit"]
+var livesLeft = document.getElementById('displayLives');
+var words = ["cat", "bird", "horse", "phone", "candle", "stamp", "wonder", "hockey", "thursday", "friday", "fruit", "banama"]
 var answer = words[Math.floor(Math.random() * words.length)];
 console.log(answer);
 var finalAnswer = answer.split('')
@@ -15,58 +14,56 @@ var numberWrongGuesses = [];
 var numberRightGuesses = [];
 
 
-var hints = ['Meow', 'Chirp', 'Nay', 'Dial me', 'In the wind', 'Sits in the corner and travels the world', " The Oneders!", "Stanley Cup", "Friday junior", "Little Saturday", "and vegetables"]
+var lives = 3;
+livesLeft.innerHTML = lives + " lives left";
+
+var hints = ['Meow', 'Chirp', 'Nay', 'Dial me', 'In the wind', 'Sits in the corner and travels the world', " The Oneders!", "Stanley Cup", "Friday junior", "Little Saturday", "and vegetables", "Curious George's favorite food"]
 
 // Where the answer will be displayed as blank spaces
 var blanks = [];
 for (i=0; i < answer.length; i++){
 	blanks[i] = " _ ";
 }
+
 console.log(blanks)
 // Display the blank spaces on the screen, .join removes the commas
 blankAnswer.innerHTML = blanks.join(' ');
-
-
-var lives =document.getElementById('displayLives');
-var lives = 3;
-displayLives.innerHTML = "You have " + lives + " more gusses";
+letterGuessed.innerHTML = "Letters Guessed: ";
 
 // click submit to guess a letter after typing one in
 submit.addEventListener('click', function(){
 	var guess = document.getElementById('playerGuess').value;
+	letterGuess.push(guess);
+	letterGuessed.innerHTML = "Letters Guessed: " + letterGuess;
 	var bool = false;
 	for (i=0; i <= finalAnswer.length; i++) {
 		if (guess === finalAnswer[i]){
 			console.log("this is correct")
 			blanks[i] = guess
 			blankAnswer.innerHTML = blanks.join(' ');
-			letterGuess.push(guess)
 			numberRightGuesses.push(guess)
-			letterGuessed.innerHTML = letterGuess;
 			bool = true;
+			if (numberRightGuesses.length === finalAnswer.length){
+				setTimeout(function() {alert('You Win!');});
+						window.location.reload()
+			}
 		}
 	}
 
 	if(!bool){
-	for (j=0; j <= finalAnswer.length; j++) {
-		if( guess != finalAnswer[i]){
-			console.log('this is incorrect')
-			letterGuess.push(guess)
-			numberWrongGuesses.push(guess)
-			letterGuessed.innerHTML = letterGuess;
-			return
-			}
+		for (j=0; j <= finalAnswer.length; j++) {
+			if( guess != finalAnswer[i]){
+				if (numberWrongGuesses.length == 2){
+					setTimeout(function(){alert('You Lose!')})
+					window.location.reload();
+				} lives--;
+			  	livesLeft.innerHTML = lives + " lives left";
+			  	numberWrongGuesses.push(guess);
+			  	console.log('this is incorrect')
+			  	return	
+			}	
 		}
 	}	
-
-	if(numberRightGuesses.length === finalAnswer.length) {
-		displayLives.innerHTML = "You Win!";
-	}
-
-	if(numberWrongGuesses.length > lives) {
-		displayLives.innerHTML = "You Lose!";
-	}
-	console.log(lives)
 })
 
 hint.addEventListener('click', function(){
